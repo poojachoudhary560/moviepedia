2c78f24ca4b49ae54f4d2df09647709d77a1620d
Pooja Choudhary <poojachoudhary560@gmail.com>
2019-08-19 00:48:28 +0530

    added CORS anywhere

--- src/components/LatestMovie.vue
+++ src/components/LatestMovie.vue
@@ -9,14 +9,17 @@ import axios from 'axios';
 export default {
   data() {
     return {
-
+      wholeResponse: [],
+      loading: true,
     }
   },
   mounted() {
-    axios.get('http://www.omdbapi.com/?apikey=b6e26482&s=avengers&page=1&type=movie&Content-Type=application/json')
+    axios.get('https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?apikey=b6e26482&s=avengers&page=1&type=movie&Content-Type=application/json')
     .then(response => {
       console.log("abc");
       console.log(response);
+      this.wholeResponse = response.data.Search;
+      this.loading = false;
     })
     .catch(error => {
       console.log("def");
