9b03903eb48f5afbc30917817103d35e019299e4
Pooja Choudhary <poojachoudhary560@gmail.com>
2019-08-19 00:31:27 +0530

    added axios get avengers

--- src/components/LatestMovie.vue
+++ src/components/LatestMovie.vue
@@ -3,7 +3,23 @@
 </template>
 
 <script>
+import axios from 'axios';
+let url = 'http://www.omdbapi.com/?s=avengers&page=1&type=movie&Content-Type=application/json';
+let API_KEY = 'b6e26482';
 export default {
-  
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    axios.get('http://www.omdbapi.com/?apikey=b6e26482&s=avengers&page=1&type=movie&Content-Type=application/json')
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  },
 }
 </script>
