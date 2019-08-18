6f7754e768fe9e914d48e76c5310348f1bc8c326
Pooja Choudhary <poojachoudhary560@gmail.com>
2019-08-19 00:38:54 +0530

    axios issues

--- src/components/LatestMovie.vue
+++ src/components/LatestMovie.vue
@@ -4,8 +4,8 @@
 
 <script>
 import axios from 'axios';
-let url = 'http://www.omdbapi.com/?s=avengers&page=1&type=movie&Content-Type=application/json';
-let API_KEY = 'b6e26482';
+//let url = 'http://www.omdbapi.com/?s=avengers&page=1&type=movie&Content-Type=application/json';
+//let API_KEY = 'b6e26482';
 export default {
   data() {
     return {
@@ -15,9 +15,11 @@ export default {
   mounted() {
     axios.get('http://www.omdbapi.com/?apikey=b6e26482&s=avengers&page=1&type=movie&Content-Type=application/json')
     .then(response => {
+      console.log("abc");
       console.log(response);
     })
     .catch(error => {
+      console.log("def");
       console.log(error);
     })
   },
